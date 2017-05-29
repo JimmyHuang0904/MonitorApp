c91dc35ce3d337baec2124ea4a6a94cb8274244c
Jimmy Huang <jihuang@sierrawireless.com>
2017-05-29 16:06:59 -0700

    Added polling timer with settable intervals

--- TrafficLightComp/TrafficLight.c
+++ TrafficLightComp/TrafficLight.c
@@ -1,12 +1,16 @@
 #include "legato.h"
 #include "le_cfg_interface.h"
+//#include "le_timer.h"
 #include <curl/curl.h>
 
 #define SSL_ERROR_HELP    "Make sure your system date is set correctly (e.g. `date -s '2016-7-7'`)"
 #define SSL_ERROR_HELP_2  "You can check the minimum date for this SSL cert to work using: `openssl s_client -connect httpbin.org:443 2>/dev/null | openssl x509 -noout -dates`"
 
-static const char * Url = "http://jenkins-legato/job/Legato-QA-Merged/lastBuild/api/json?tree=result";
+static const char * Url = "http://jenkins-legato/job/Legato-Review/lastBuild/api/json?tree=result";
+static const int seconds = 3;
 // http://jenkins-legato/ or http://10.1.11.48 //Mdm9x06-Manifest-Merged
+
+le_cfg_IteratorRef_t iteratorRef;
 
 struct MemoryStruct{
 	char *memory;
@@ -117,6 +121,66 @@ static void GetUrl
 
     curl_global_cleanup();
 }
+
+// static void CfgTreeSet
+// (
+//     void
+// )
+// {
+
+//     le_cfg_IteratorRef_t iteratorRef = le_cfg_CreateWriteTxn("TrafficLight:/test");
+//     le_cfg_SetBool(iteratorRef, "isBoolSet", true);
+
+//     ///////
+//     le_cfg_CreateReadTxn("TrafficLight:/test");
+//     bool myBoolVal = le_cfg_GetBool(iteratorRef, "isBoolSet", false);
+
+//     LE_INFO("myBoolVal=%d", myBoolVal);
+
+//     if (myBoolVal)
+//     {
+//         LE_INFO("The test value was set.");
+//     }
+//     else
+//     {
+//         LE_INFO("The test value was not set.");
+//     }
+
+// }
+
+// static void CfgTreeGet
+// (
+//     void
+// )
+// {
+
+//     le_cfg_CreateReadTxn("TrafficLight:/test");
+//     bool myBoolVal = le_cfg_GetBool(iteratorRef, "isBoolSet", false);
+
+//     float myFloatVal = le_cfg_GetFloat(iteratorRef, "isFloatSet", 3);
+
+//     LE_INFO("myBoolVal=%d, myFloatVal=%f", myBoolVal, myFloatVal);
+
+//     if (myBoolVal)
+//     {
+//         LE_INFO("The test value was set.");
+//     }
+//     else
+//     {
+//         LE_INFO("The test value was not set.");
+//     }
+// }
+static void Polling
+(
+    le_timer_Ref_t timerRef
+)
+{
+    LE_INFO("-------------------------- In polling function--------------------");    
+
+    GetUrl();
+
+}
+
 //---------------------------------------------------
 /**
  
@@ -126,16 +190,14 @@ COMPONENT_INIT
 {
 	curl_global_init(CURL_GLOBAL_ALL);  //maybe parameter can just be CURL_GLOBAL_SSL
 
- 	GetUrl();
-    
-    le_cfg_IteratorRef_t iteratorRef = le_cfg_CreateReadTxn("/");
-    bool myBoolVal = le_cfg_GetBool(iteratorRef, "TestBool", false);
-    if (myBoolVal)
-    {
-        LE_INFO("The test value was set.");
-    }
-    else
-    {
-        LE_INFO("The test value was not set.");
-    }
+    //CfgTreeSet();
+    //CfgTreeGet();
+
+    le_timer_Ref_t PollingTimer = le_timer_Create("PollingTimer");
+    le_clk_Time_t interval = {seconds, 0}; //first parameter is the seconds
+    le_timer_SetInterval(PollingTimer, interval);
+    le_timer_SetRepeat(PollingTimer, 0); // repeat indefinitely
+    le_timer_SetHandler(PollingTimer, Polling);
+    le_timer_Start(PollingTimer);
+
 }
